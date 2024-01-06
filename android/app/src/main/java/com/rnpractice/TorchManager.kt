fd9fc162b302e69de16309a07ad4652839dfdda9
Wayne Weng <wayne.weng@shoalter.com>
2024-01-06 16:44:38 +0800

    formatting android code

--- android/app/src/main/java/com/rnpractice/TorchManager.kt
+++ android/app/src/main/java/com/rnpractice/TorchManager.kt
@@ -1,4 +1,5 @@
 package com.rnpractice
+
 import android.content.Context
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraManager
@@ -6,11 +7,16 @@ import android.os.Build
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-class TorchManager(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext)  {
+
+class TorchManager(private val reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "TorchManager"
-    @ReactMethod fun toggleTorch(enabled: Boolean) {
+
+    @ReactMethod
+    fun toggleTorch(enabled: Boolean) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val cameraManager = reactContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            val cameraManager =
+                reactContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager
             try {
                 val cameraId = cameraManager.cameraIdList[0]
                 cameraManager.setTorchMode(cameraId, enabled)
